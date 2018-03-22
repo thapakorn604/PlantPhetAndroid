cc7d6b4303ed62ac70b64b8092a8d3bbd642d627
ploychanok <plo_y@live.com>
2018-03-22 12:48:36 +0700

    อัพผิด

--- PlantPhetApp/src/actions/AppActions.js
+++
@@ -1,5 +0,0 @@
-/* @flow */
-
-export const sayHi = () => {
-  return { type: 'SAY_HI', payload: 'Hi' };
-};
--- PlantPhetApp/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-import { combineReducers } from 'redux';
-import AppReducers from './AppReducers';
-
-const rootReducer = combineReducers({
-  app: AppReducers
-});
-
-export default rootReducer;

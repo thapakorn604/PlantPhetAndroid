99b6c65d9b0a7f13f52fb9784815f2f2c933b27e
Lionking2001 <kitsada_tang@cmu.ac.th>
2018-04-02 20:08:50 +0700

    Get Current LAT LONG NOt work on emulator

--- PlantPhetApp/PlantPhetFarmer/app/src/main/java/com/example/ploychanok/plantphetfarmer/MapActivity.java
+++ PlantPhetApp/PlantPhetFarmer/app/src/main/java/com/example/ploychanok/plantphetfarmer/MapActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.SyncStateContract;
@@ -27,14 +29,33 @@ public class MapActivity extends AppCompatActivity {
     MyLocationNewOverlay mLocationOverlay;
     MapView map = null;
     Context context;
+    LocationManager locationManager;
+    double longitude;
+    double latitude;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
+
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+
 
         setContentView(R.layout.activity_map);
 
@@ -45,7 +66,8 @@ public class MapActivity extends AppCompatActivity {
         IMapController mapController = map.getController();
         mapController.setZoom(10.2);
 
-        GeoPoint startPoint = new GeoPoint(18.796143, 98.979263);
+        GeoPoint startPoint = new GeoPoint(latitude, longitude);
+
 
         addMarker(startPoint);
         mapController.setCenter(startPoint);
@@ -87,7 +109,9 @@ public class MapActivity extends AppCompatActivity {
 
 
 
+
     }
+
 
 
 }

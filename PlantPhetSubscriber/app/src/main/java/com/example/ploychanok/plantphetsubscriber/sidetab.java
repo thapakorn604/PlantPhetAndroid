0dfaeaafd33881f796eecc82c840e77f11b7f4aa
ploychanok <plo_y@live.com>
2018-04-29 11:29:12 +0700

    fix error

--- PlantPhetSubscriber/app/src/main/java/com/example/ploychanok/plantphetsubscriber/sidetab.java
+++ PlantPhetSubscriber/app/src/main/java/com/example/ploychanok/plantphetsubscriber/sidetab.java
@@ -80,17 +80,13 @@ public class sidetab extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_account) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.help) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout) {
 
         }
 

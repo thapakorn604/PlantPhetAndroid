69aad31dfc12384d162097608ea9139a7ae3c0e8
ploychanok <plo_y@live.com>
2018-05-05 14:42:29 +0700

    บึ้มอยู่

---
+++ PlantPhetApp/Subscriber/app/src/main/java/com/example/ploychanok/subscriber/homeActivity.java
@@ -0,0 +1,44 @@
+package com.example.ploychanok.subscriber;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class homeActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        auth = FirebaseAuth.getInstance();
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(homeActivity.this, login.class));
+                    finish();
+                }
+            }
+        };
+    }
+
+
+
+    public void signOut() {
+        auth.signOut();
+    }
+}

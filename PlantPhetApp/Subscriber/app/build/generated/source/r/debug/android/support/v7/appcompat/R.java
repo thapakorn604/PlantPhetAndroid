e59be7eb5bc6afbe8b44042ecc7c46dcccf15ce2
ploychanok <plo_y@live.com>
2018-05-06 18:41:35 +0700

    add aboutus

--- PlantPhetApp/Subscriber/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ PlantPhetApp/Subscriber/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -686,15 +686,15 @@ public final class R {
         public static final int text2 = 0x7f0900d6;
         public static final int textSpacerNoButtons = 0x7f0900d7;
         public static final int textSpacerNoTitle = 0x7f0900d8;
-        public static final int time = 0x7f0900dd;
-        public static final int title = 0x7f0900de;
-        public static final int titleDividerNoCustom = 0x7f0900df;
-        public static final int title_template = 0x7f0900e2;
-        public static final int top = 0x7f0900e3;
-        public static final int topPanel = 0x7f0900e4;
-        public static final int uniform = 0x7f0900eb;
-        public static final int up = 0x7f0900ec;
-        public static final int wrap_content = 0x7f0900f5;
+        public static final int time = 0x7f0900de;
+        public static final int title = 0x7f0900df;
+        public static final int titleDividerNoCustom = 0x7f0900e0;
+        public static final int title_template = 0x7f0900e3;
+        public static final int top = 0x7f0900e4;
+        public static final int topPanel = 0x7f0900e5;
+        public static final int uniform = 0x7f0900ec;
+        public static final int up = 0x7f0900ed;
+        public static final int wrap_content = 0x7f0900f6;
     }
     public static final class integer {
         public static final int abc_config_activityDefaultDur = 0x7f0a0000;
@@ -731,16 +731,16 @@ public final class R {
         public static final int abc_search_view = 0x7f0b0018;
         public static final int abc_select_dialog_material = 0x7f0b0019;
         public static final int abc_tooltip = 0x7f0b001a;
-        public static final int notification_action = 0x7f0b0032;
-        public static final int notification_action_tombstone = 0x7f0b0033;
-        public static final int notification_template_custom_big = 0x7f0b003a;
-        public static final int notification_template_icon_group = 0x7f0b003b;
-        public static final int notification_template_part_chronometer = 0x7f0b003f;
-        public static final int notification_template_part_time = 0x7f0b0040;
-        public static final int select_dialog_item_material = 0x7f0b0043;
-        public static final int select_dialog_multichoice_material = 0x7f0b0044;
-        public static final int select_dialog_singlechoice_material = 0x7f0b0045;
-        public static final int support_simple_spinner_dropdown_item = 0x7f0b0046;
+        public static final int notification_action = 0x7f0b0033;
+        public static final int notification_action_tombstone = 0x7f0b0034;
+        public static final int notification_template_custom_big = 0x7f0b003b;
+        public static final int notification_template_icon_group = 0x7f0b003c;
+        public static final int notification_template_part_chronometer = 0x7f0b0040;
+        public static final int notification_template_part_time = 0x7f0b0041;
+        public static final int select_dialog_item_material = 0x7f0b0044;
+        public static final int select_dialog_multichoice_material = 0x7f0b0045;
+        public static final int select_dialog_singlechoice_material = 0x7f0b0046;
+        public static final int support_simple_spinner_dropdown_item = 0x7f0b0047;
     }
     public static final class string {
         public static final int abc_action_bar_home_description = 0x7f0d0000;

fda4b5818924631eae92a839abe1a8b955b9e991
ploychanok <plo_y@live.com>
2018-05-10 03:20:01 +0700

    บัคจ้า บัคร้อนๆ

--- PlantPhetApp/NewProject/app/src/main/java/com/example/msiraider/newproject/Register/RegisFatmer.java
+++ PlantPhetApp/NewProject/app/src/main/java/com/example/msiraider/newproject/Register/RegisFatmer.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.msiraider.newproject.HomeActivity;
